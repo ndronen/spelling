37026da31e964c60d877a2610835872c13149b16
Nicholas Dronen <nicholas.dronen@pearson.com>
2016-01-08 11:13:57 -0700

    Test for NorvigWithAspellDictGoogleLanguageModel

--- tests/testdictionary.py
+++ tests/testdictionary.py
@@ -10,26 +10,32 @@ import spelling.dictionary
 from spelling.dictionary import NORVIG_DATA_PATH as train_path
 
 class TestDictionary(unittest.TestCase):
-    def test_enchant(self):
-        d = spelling.dictionary.Enchant()
+    def test_aspell(self):
+        d = spelling.dictionary.Aspell()
         self.assertTrue(d.check("quick"))
         suggestions = d.suggest("quickq")
         self.assertTrue("quick" in suggestions)
 
     def test_norvig(self):
-        d = spelling.dictionary.Norvig(train_path=train_path)
+        d = spelling.dictionary.Norvig()
         self.assertTrue(d.check("quick"))
         suggestions = d.suggest("quickq")
         self.assertTrue("quick" in suggestions)
 
-    def test_norvig_without_language_model(self):
-        d = spelling.dictionary.NorvigWithoutLanguageModel(train_path=train_path)
+    def test_norvig_without_norvig_language_model(self):
+        d = spelling.dictionary.NorvigWithoutNorvigLanguageModel()
         self.assertTrue(d.check("quick"))
         suggestions = d.suggest("quickq")
         self.assertTrue("quick" in suggestions)
 
-    def test_enchant_with_language_model(self):
-        d = spelling.dictionary.EnchantWithLanguageModel(train_path=train_path)
+    def test_aspell_with_norvig_language_model(self):
+        d = spelling.dictionary.AspellWithNorvigLanguageModel()
+        self.assertTrue(d.check("quick"))
+        suggestions = d.suggest("quickq")
+        self.assertTrue("quick" in suggestions)
+
+    def test_norvig_with_aspell_dict_google_language_model(self):
+        d = spelling.dictionary.NorvigWithAspellDictGoogleLanguageModel()
         self.assertTrue(d.check("quick"))
         suggestions = d.suggest("quickq")
         self.assertTrue("quick" in suggestions)
