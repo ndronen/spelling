e71c6d20631ec0618ea80de6a1d924db47ba9f0c
Nicholas Dronen <nicholas.dronen@pearson.com>
2016-03-04 13:14:51 -0700

    Make spelling.utils.build_progressbar take either int or list

--- spelling/utils.py
+++ spelling/utils.py
@@ -1,7 +1,11 @@
 import progressbar
 
 def build_progressbar(items):
+    if isinstance(items, int):
+        maxval = items
+    else:
+        maxval = len(items)
     return progressbar.ProgressBar(term_width=40,
         widgets=[' ', progressbar.Percentage(),
         ' ', progressbar.ETA()],
-        maxval=len(items)).start()
+        maxval=maxval).start()
