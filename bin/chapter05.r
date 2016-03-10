b797026fd4cc645bc879a67288771bfeeb079bd2
Nicholas Dronen <nicholas.dronen@pearson.com>
2016-03-10 12:24:29 -0700

    Snapshot of plotting code for chapter 5

--- bin/chapter05.r
+++ bin/chapter05.r
@@ -31,26 +31,33 @@ colnames(df) <- str_replace_all(colnames(df), "_", " ")
 colnames(df) <- capitalize(colnames(df))
 print(names(df))
 
-# The distributions of word lengths.
-#pdf("freq-dist-non-word-length.pdf")
-#print(histogram(~Length | Corpus, data=df, xlab="Non-word length"))
-#dev.off()
-
-# The distributions of edit distances from the non-word to the true correction.
-#pdf("freq-dist-levenshtein-distance.pdf")
-#print(histogram(~`Levenshtein distance` | Corpus, data=df))
-#dev.off()
+# The distributions of word lengths and of edit distances from the
+# non-word to the true correction.
+pdf("freq-dist-length-distance.pdf")
+dist_length <- histogram(~Length | Corpus,
+    data=df,
+    xlab="Non-word length")
+dist_levenshtein <- histogram(~`Levenshtein distance` | Corpus,
+    data=df,
+    xlab="Levenshtein distance to correction")
+print(grid.arrange(dist_length, dist_levenshtein, nrow=2))
+dev.off()
 
 # The distributions of lengths of candidate lists -- need to do this
 # for Aspell and Edit distance retrievers.
-graphics.off()
 pdf("freq-dist-candidate-lists.pdf")
 ed <- histogram(~`Edit distance n candidates` | Corpus,
           data=df,
-          xlab="Length of candidate list (Edit distance)")
+          xlab="Near-miss retrieval")
 aspell <- histogram(~`Aspell n candidates` | Corpus,
           data=df,
-          xlab="Length of candidate list (Aspell)",
+          xlab="Aspell retrieval",
           ylab=NULL)
 print(grid.arrange(ed, aspell, ncol=2))
 dev.off()
+
+# For a given corpus and dictionary, what is the distribution of the
+# ranks of true corrections in the candidate list for each error?
+
+# What is that distribution conditioned on (1) the length of the non-word
+# or (2) the edit distance from the non-word to its correction?
